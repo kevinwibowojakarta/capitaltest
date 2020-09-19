ed2baa9fef26dca6079dce3a79407a45a6465e44
Kevin Wibowo <wibowok@dnid0708l01.local>
2020-09-19 14:45:04 +0700

    Add gitignore and brightness testcase

--- src/test/java/capitaltest/test/MainTest.java
+++ src/test/java/capitaltest/test/MainTest.java
@@ -3,22 +3,29 @@ package capitaltest.test;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class MainTest {
 	public AndroidDriver<MobileElement> driver;
 	public WebDriverWait wait;
+	public int screenHeight;
+	public int screenWidth;
 	
 	@BeforeSuite
 	public void initiate() throws IOException {
 		//Initiate the AndroidDriver
-		Runtime.getRuntime().exec(new String[]{"bash", "-l", "-c", "emulator -avd capital_test"});
+		
 		
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("deviceName", "capital_test");
@@ -32,18 +39,42 @@ public class MainTest {
 		
 		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
 		wait = new WebDriverWait(driver, 30);
+		screenHeight = driver.manage().window().getSize().getHeight();
+		screenWidth = driver.manage().window().getSize().getWidth();
 
 	}
 	
-	@Test
-	public void singleTest() {
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("opened the app");
+	@Test(description="Adjust Brightness")
+	public void test1() {
+		//Open Quick Setting
+		TouchAction ts = new TouchAction(this.driver);
+		int startX = (int) (this.screenWidth * 0.5);
+		int startY = (int) (1);
+		int endX = (int) (this.screenWidth * 0.5);
+		int endY = (int) (this.screenHeight * 0.5);
+		ts.longPress(PointOption.point(startX, startY)).moveTo(PointOption.point(endX,endY)).release().perform();
+		//Open Expanded Quick Setting
+		ts.longPress(PointOption.point(startX, startY)).moveTo(PointOption.point(endX,endY)).release().perform();
+		//Get the slider element
+		MobileElement brightness_slider = driver.findElementById("com.android.systemui:id/slider");
+		//Calculate the distance
+		int slider_location_x=brightness_slider.getLocation().getX();
+		int slider_location_y=brightness_slider.getLocation().getY();
+		int slider_size_x=brightness_slider.getSize().getWidth();
+		int slider_size_y=brightness_slider.getSize().getHeight();
+		//Calculate the coordinate
+		System.out.println(slider_location_x);
+		System.out.println(slider_location_y);
+		System.out.println(slider_size_x);
+		System.out.println((slider_location_y+slider_size_y)/2);
+		//Move the slider to 0
+		ts.tap(PointOption.point(slider_location_x, (slider_location_y+(slider_size_y/2)))).perform();
+		//Move the slider from 0 to 100 in 10 second
+		ts.longPress(PointOption.point(slider_location_x, (slider_location_y+(slider_size_y/2)))).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(10)))
+		.moveTo(PointOption.point(slider_location_x+slider_size_x, (slider_location_y+(slider_size_y/2)))).release().perform();
+		
+		
+		System.out.println("Test 1 Done");
 		driver.quit();
 	}
 	
